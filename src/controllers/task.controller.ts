fbc1bfd715a40bfa483b3f5bd31d188896daaa32
Pedro Rossini <rossinipedro613@gmail.com>
2026-07-04 13:05:53 -0400

    feat: add GET /tasks/:id route and controller

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -8,16 +8,49 @@ interface CreateTaskBody {
 }
 
 class TaskController {
+    static async getOne(req: Request, res: Response) {
+        const id = req.params.id;
+
+        try {
+            const task = await Task.findById(id);
+
+            if (!task) {
+                return res.status(404).json({
+                    message: "There is no task with that ID.",
+                    success: false
+                });
+            }
+
+            return res.status(200).json({
+                message: "Task search completed successfully.",
+                success: true,
+                data: task
+            });
+        } catch (error) {
+            if (error instanceof Error && error.name == "CastError") {
+                return res.status(400).json({
+                    message: "Invalid ID format.",
+                    success: false
+                });
+            }
+
+            return res.status(500).json({
+                message: "Internal server error.",
+                success: false
+            });
+        }
+    }
+
     static async getAll(req: Request, res: Response) {
         try {
             const allTasks = await Task.find();
 
             return res.status(200).json({
-                message: "Task search completed successfully.",
+                message: "Tasks search completed successfully.",
                 success: true,
                 data: allTasks
             });
-        } catch(error) {
+        } catch (error) {
             return res.status(500).json({
                 message: "Internal server error.",
                 success: false
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 
 const taskRoutes = Router();
 
+taskRoutes.get("/tasks/:id", TaskController.getOne);
 taskRoutes.get("/tasks", TaskController.getAll);
 taskRoutes.post("/tasks", TaskController.create);
 
